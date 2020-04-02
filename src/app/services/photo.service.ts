10b92dd1965c83a653cffd8475e3b22fc00ae33e
Alvin Mukiibi <mukikelly792@gmail.com>
2020-04-02 22:23:43 +0300

    photo display

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PhotoService {
+  public photos: Photo[] = [];
 
-  constructor() { }
+  currentImage: any; // declare an attribute on the class and since its typescript, give it a type of any
+
+  constructor(private camera: Camera) {}
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.currentImage = "data:image/jpeg;base64," + imageData;
+        this.photos.unshift({
+          data: this.currentImage
+        });
+      },
+      err => {
+        console.log("Camera Issue: " + err);
+      }
+    );
+  }
+}
+
+class Photo {
+  data: any;
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,33 +1,13 @@
-import { Component } from '@angular/core';
+import { PhotoService } from "./../services/photo.service";
+import { Component } from "@angular/core";
 // import the Camera and Options components
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx'
-
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"]
 })
 export class Tab2Page {
-
-  currentImage: any; // declare an attribute on the class and since its typescript, give it a type of any
-
   // initialise a camera variable to the Camera component
-  constructor(private camera: Camera) { }
-
-  takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    };
-
-    this.camera.getPicture(options).then(imageData => {
-      this.currentImage = 'data:image/jpeg;base64,' + imageData;
-    }, err => {
-      console.log("Camera Issue: " + err);
-    });
-  }
-
+  constructor(public photoService: PhotoService) {}
 }
